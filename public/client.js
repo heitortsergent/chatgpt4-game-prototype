ca5b117ce4ef38c8d6cd2fa8363a7acf4f62ed05
Heitor Tashiro Sergent <heitortsergent@gmail.com>
2023-03-18 10:56:45 -0300

    Add room functionality

--- public/client.js
+++ public/client.js
@@ -1,134 +1,153 @@
-const socket = io();
+const roomInput = document.getElementById('roomInput');
+const createRoomBtn = document.getElementById('createRoomBtn');
+const gameContainer = document.getElementById('gameContainer');
 
-const config = {
-  type: Phaser.AUTO,
-  width: 800,
-  height: 600,
-  backgroundColor: '#8cac0e',
-  physics: {
-    default: 'arcade',
-    arcade: {
-      gravity: { y: 0 },
+createRoomBtn.addEventListener('click', () => {
+    const roomName = roomInput.value.trim();
+    if (roomName.length > 0) {
+        createRoom(roomName);
     }
-  },
-  scene: {
-    preload: preload,
-    create: create,
-    update: update
-  }
-};
+});
 
-const game = new Phaser.Game(config);
-let player;
-let otherPlayers;
-let boxes;
-let cursors;
-let floor;
+function createRoom(roomName) {
+  gameContainer.style.display = 'block';
+  roomInput.style.display = 'none';
+  createRoomBtn.style.display = 'none';
 
-function preload() {
-  this.load.image('player', 'assets/character.png');
-  this.load.image('box', 'assets/box.png');
-}
+  // Connect to the server and join the room
+  const socket = io();
+  socket.emit('joinRoom', roomName);
 
-function create() {
-  console.log('client create function');
-  player = this.physics.add.sprite(config.width / 2, config.height - 50, 'player');
-  otherPlayers = this.add.group();
-  cursors = this.input.keyboard.createCursorKeys();
-
-  socket.on('currentPlayers', (players) => {
-    Object.keys(players).forEach((id) => {
-        if (id !== socket.id) {
-            const otherPlayer = this.add.sprite(players[id].x, players[id].y, 'player');
-            otherPlayer.playerId = id;
-            otherPlayers.add(otherPlayer);
-        }
-    });
-  });
-
-  socket.on('currentBoxes', (newBoxes) => {
-    newBoxes.forEach((box) => {
-      console.log(box);
-      boxes.create(box.boxInfo.x, box.boxInfo.y, 'box');
-    });
-  });
-
-  socket.on('newPlayer', (playerInfo) => {
-    console.log('socket on newPlayer');
-    const otherPlayer = this.add.sprite(playerInfo.x, playerInfo.y, 'player');
-    otherPlayer.playerId = playerInfo.playerId;
-    otherPlayers.add(otherPlayer);
-  });
-
-  socket.on('playerMoved', (playerInfo) => {
-    otherPlayers.getChildren().forEach((otherPlayer) => {
-      if (playerInfo.playerId === otherPlayer.playerId) {
-        otherPlayer.setPosition(playerInfo.x, playerInfo.y);
+  const config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    backgroundColor: '#8cac0e',
+    physics: {
+      default: 'arcade',
+      arcade: {
+        gravity: { y: 0 },
       }
-    });
-  });
-
-  socket.on('playerDisconnected', (playerId) => {
-    otherPlayers.getChildren().forEach((otherPlayer) => {
-      if (playerId === otherPlayer.playerId) {
-        otherPlayer.destroy();
-      }
-    });
-  });
-
-  socket.on('boxCreated', (boxInfo) => {
-    boxes.create(boxInfo.x, boxInfo.y, 'box');
-  });
-
-  player.setCollideWorldBounds(true);
-
-  boxes = this.physics.add.group();
-
-  cursors = this.input.keyboard.createCursorKeys();
-  this.input.keyboard.on('keydown-SPACE', placeBox, this);
-
-  // Add this code to create the floor
-  floor = this.physics.add.staticGroup();
-  const floorRect = this.add.rectangle(config.width / 2, config.height - 25, config.width, 5, 0x000000);
-  floor.add(floorRect);
-  this.physics.add.collider(player, floor);
-
-  socket.emit('clientReady');
-}
-
-function update() {
-  if (cursors.left.isDown) {
-    player.setVelocityX(-200);
-  } else if (cursors.right.isDown) {
-    player.setVelocityX(200);
-  } else {
-    player.setVelocityX(0);
-  }
-
-  if (cursors.up.isDown) {
-    player.setVelocityY(-200);
-  } else if (cursors.down.isDown) {
-    player.setVelocityY(200);
-  } else {
-    player.setVelocityY(0);
-  }
-
-  // Emit player movement
-  const x = player.x;
-  const y = player.y;
-
-  if (player.oldPosition && (x !== player.oldPosition.x || y !== player.oldPosition.y)) {
-    socket.emit('playerMovement', { x: player.x, y: player.y });
-  }
-
-  // Save old position data
-  player.oldPosition = {
-    x: player.x,
-    y: player.y,
+    },
+    scene: {
+      preload: preload,
+      create: create,
+      update: update
+    }
   };
-}
-
-function placeBox() {
-  const box = boxes.create(player.x, player.y - 32, 'box');
-  socket.emit('boxCreated', { x: box.x, y: box.y });
+  
+  const game = new Phaser.Game(config);
+  let player;
+  let otherPlayers;
+  let boxes;
+  let cursors;
+  let floor;
+  
+  function preload() {
+    this.load.image('player', 'assets/character.png');
+    this.load.image('box', 'assets/box.png');
+  }
+  
+  function create() {
+    player = this.physics.add.sprite(config.width / 2, config.height - 50, 'player');
+    otherPlayers = this.add.group();
+    cursors = this.input.keyboard.createCursorKeys();
+  
+    socket.on('currentPlayers', (players) => {
+      console.log('socket on currentPlayers');
+      Object.keys(players).forEach((id) => {
+          if (id !== socket.id) {
+              const otherPlayer = this.add.sprite(players[id].x, players[id].y, 'player');
+              otherPlayer.playerId = id;
+              otherPlayers.add(otherPlayer);
+          }
+      });
+    });
+  
+    socket.on('currentBoxes', (newBoxes) => {
+      newBoxes.forEach((box) => {
+        boxes.create(box.boxInfo.x, box.boxInfo.y, 'box');
+      });
+    });
+  
+    socket.on('newPlayer', (playerInfo) => {
+      console.log('socket on newPlayer');
+      const otherPlayer = this.add.sprite(playerInfo.x, playerInfo.y, 'player');
+      otherPlayer.playerId = playerInfo.playerId;
+      otherPlayers.add(otherPlayer);
+    });
+  
+    socket.on('playerMoved', (playerInfo) => {
+      otherPlayers.getChildren().forEach((otherPlayer) => {
+        if (playerInfo.playerId === otherPlayer.playerId) {
+          otherPlayer.setPosition(playerInfo.x, playerInfo.y);
+        }
+      });
+    });
+  
+    socket.on('playerDisconnected', (playerId) => {
+      otherPlayers.getChildren().forEach((otherPlayer) => {
+        if (playerId === otherPlayer.playerId) {
+          otherPlayer.destroy();
+        }
+      });
+    });
+  
+    socket.on('boxCreated', (boxInfo) => {
+      boxes.create(boxInfo.x, boxInfo.y, 'box');
+    });
+  
+    player.setCollideWorldBounds(true);
+  
+    boxes = this.physics.add.group();
+  
+    cursors = this.input.keyboard.createCursorKeys();
+    this.input.keyboard.on('keydown-SPACE', placeBox, this);
+  
+    // Add this code to create the floor
+    floor = this.physics.add.staticGroup();
+    const floorRect = this.add.rectangle(config.width / 2, config.height - 25, config.width, 5, 0x000000);
+    floor.add(floorRect);
+    this.physics.add.collider(player, floor);
+    
+    // Send event for server to add existing players and boxes to new player
+    socket.emit('clientReady');
+  }
+  
+  function update() {
+    if (cursors.left.isDown) {
+      player.setVelocityX(-200);
+    } else if (cursors.right.isDown) {
+      player.setVelocityX(200);
+    } else {
+      player.setVelocityX(0);
+    }
+  
+    if (cursors.up.isDown) {
+      player.setVelocityY(-200);
+    } else if (cursors.down.isDown) {
+      player.setVelocityY(200);
+    } else {
+      player.setVelocityY(0);
+    }
+  
+    // Emit player movement
+    const x = player.x;
+    const y = player.y;
+  
+    if (player.oldPosition && (x !== player.oldPosition.x || y !== player.oldPosition.y)) {
+      socket.emit('playerMovement', { x: player.x, y: player.y });
+    }
+  
+    // Save old position data
+    player.oldPosition = {
+      x: player.x,
+      y: player.y,
+    };
+  }
+  
+  function placeBox() {
+    const box = boxes.create(player.x, player.y - 32, 'box');
+    socket.emit('boxCreated', { x: box.x, y: box.y });
+  }
 }
--- server.js
+++ server.js
@@ -15,41 +15,64 @@ server.listen(8080, () => {
 });
 
 const players = {};
-const boxes = [];
+const boxes = {};
 
 io.on('connection', (socket) => {
   console.log('A user connected:', socket.id);
 
-  players[socket.id] = {
-    playerId: socket.id,
-    x: Math.random() * 800,
-    y: Math.random() * 600,
-  };
+  socket.on('joinRoom', (roomName) => {
+    socket.join(roomName);
+    // Add the player to the players object
+    players[socket.id] = {
+        playerId: socket.id,
+        x: Math.floor(Math.random() * 700) + 50,
+        y: Math.floor(Math.random() * 500) + 50,
+        room: roomName
+    };
+
+    // Initialize the boxes array for the room if it does not exist
+    if (!boxes[roomName]) {
+      boxes[roomName] = [];
+    }
+  
+    // socket.on('clientReady', () => {
+    //   console.log('socket on clientReady');
+    //   socket.to(roomName).emit('currentPlayers', players);
+  
+    //   socket.to(roomName).emit('currentBoxes', boxes[roomName]);
+    // });
+
+    // socket.emit('currentPlayers', players);
+    // socket.emit('currentBoxes', boxes[roomName]);
+  
+    // io.emit('newPlayer', players[socket.id]);
+    socket.to(roomName).emit('newPlayer', players[socket.id]);
+  
+    socket.on('playerMovement', (movementData) => {
+      players[socket.id].x = movementData.x;
+      players[socket.id].y = movementData.y;
+      socket.to(roomName).emit('playerMoved', players[socket.id]);
+    });
+  
+    socket.on('boxCreated', (boxInfo) => {
+      boxes[roomName].push({boxInfo});
+  
+      // Broadcast the new box's position to all connected clients
+      socket.to(roomName).emit('boxCreated', boxInfo);
+    });
+  
+    socket.on('disconnect', () => {
+      console.log('A user disconnected:', socket.id);
+      delete players[socket.id];
+      socket.to(roomName).emit('playerDisconnected', socket.id);
+    });
+  });
 
   socket.on('clientReady', () => {
-    socket.emit('currentPlayers', players);
-
-    socket.emit('currentBoxes', boxes);
-  });
-
-  io.emit('newPlayer', players[socket.id]);
-
-  socket.on('playerMovement', (movementData) => {
-    players[socket.id].x = movementData.x;
-    players[socket.id].y = movementData.y;
-    socket.broadcast.emit('playerMoved', players[socket.id]);
-  });
-
-  socket.on('boxCreated', (boxInfo) => {
-    boxes.push({boxInfo});
-
-    // Broadcast the new box's position to all connected clients
-    socket.broadcast.emit('boxCreated', boxInfo);
-  });
-
-  socket.on('disconnect', () => {
-    console.log('A user disconnected:', socket.id);
-    delete players[socket.id];
-    io.emit('playerDisconnected', socket.id);
+    console.log('socket on clientReady');
+    const roomName = players[socket.id].room;
+    const roomPlayers = Object.fromEntries(Object.entries(players).filter(([_, player]) => player.room === roomName));
+    socket.emit('currentPlayers', roomPlayers);
+    socket.emit('currentBoxes', boxes[roomName]);
   });
 });
